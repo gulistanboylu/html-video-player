e2e3ac2e2db5d6c18d7f86eaa67010a7b7eff646
Gulistan Boylu <gulistan.boylu@gmail.com>
2017-10-12 23:10:18 +0100

    Fix queryselector

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Get our elements to the page
 const player = document.querySelector('.player');
-const video = player.querySelector('viewer');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
